c9f7391acb086cb788505b70bde72910a3ac9983
miskiabdirizak <miskiabdirizak02@gmail.com>
2023-04-06 15:09:36 -0400

    updated files

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -1,2 +1,11 @@
 //add sql connection + code here 
 
+import mysql from "mysql"
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'password',
+  database : 'account'
+});
+export default connection
+
