5c8a7dd61d78ac5365910e3a6c0245c58c893436
Ruqayyah <40611051+rukky022@users.noreply.github.com>
2023-04-19 20:30:38 -0400

    Insert Line statements

---
+++ SQL/Insert Line.sql
@@ -0,0 +1,8 @@
+INSERT INTO Line VALUES(id, "blue", 'w');
+INSERT INTO Line VALUES(id, "blue", 'e');
+INSERT INTO Line VALUES(id, "green", 'w');
+INSERT INTO Line VALUES(id, "green", 'e');
+INSERT INTO Line VALUES(id, "red", 's');
+INSERT INTO Line VALUES(id, "red", 'n');
+INSERT INTO Line VALUES(id, "gold", 's');
+INSERT INTO Line VALUES(id, "gold", 'n');
