63684bdb56ec22a244e618a9810a86ed675c46ed
Ruqayyah <40611051+rukky022@users.noreply.github.com>
2023-04-19 20:25:41 -0400

    Updated Create and Insert statements

---
+++ SQL/Insert StationHasRoute.sql
@@ -0,0 +1,139 @@
+--  blue west
+INSERT INTO StationHasRoute VALUES("Indian Creek", 1, "st_1");
+INSERT INTO StationHasRoute VALUES ('Kensington', 1, 'st_2');
+INSERT INTO StationHasRoute VALUES ('Avondale', 1, 'st_3');
+INSERT INTO StationHasRoute VALUES ('Decatur', 1, 'st_4');
+INSERT INTO StationHasRoute VALUES ('East Lake', 1, 'st_5');
+INSERT INTO StationHasRoute VALUES ('Candler Park', 1, 'st_6');
+INSERT INTO StationHasRoute VALUES ('Inman Park/Reynoldstown', 1, 'st_7');
+INSERT INTO StationHasRoute VALUES ('King Memorial', 1, 'st_8');
+INSERT INTO StationHasRoute VALUES ('Georgia State', 1, 'st_9');
+INSERT INTO StationHasRoute VALUES ('Five Points', 1, 'st_10');
+INSERT INTO StationHasRoute VALUES ('GWCC/Mercedes-Benz Stadium', 1, 'st_11');
+INSERT INTO StationHasRoute VALUES ('Vine City', 1, 'st_12');
+INSERT INTO StationHasRoute VALUES ('Ashby', 1, 'st_13');
+INSERT INTO StationHasRoute VALUES ('West Lake', 1, 'st_14');
+INSERT INTO StationHasRoute VALUES("H.E. Holmes", 1, "st_15");
+
+
+-- blue east
+INSERT INTO StationHasRoute VALUES("H.E. Holmes", 2, "st_1");
+INSERT INTO StationHasRoute VALUES ('West Lake', 2, 'st_2');
+INSERT INTO StationHasRoute VALUES ('Ashby', 2, 'st_3');
+INSERT INTO StationHasRoute VALUES ('Vine City', 2, 'st_4');
+INSERT INTO StationHasRoute VALUES ('GWCC/Mercedes-Benz Stadium', 2, 'st_5');
+INSERT INTO StationHasRoute VALUES ('Five Points', 2, 'st_6');
+INSERT INTO StationHasRoute VALUES ('Georgia State', 2, 'st_7');
+INSERT INTO StationHasRoute VALUES ('King Memorial', 2, 'st_8');
+INSERT INTO StationHasRoute VALUES ('Inman Park/Reynoldstown', 2, 'st_9');
+INSERT INTO StationHasRoute VALUES ('Candler Park', 2, 'st_10');
+INSERT INTO StationHasRoute VALUES ('East Lake', 2, 'st_11');
+INSERT INTO StationHasRoute VALUES ('Decatur', 2, 'st_12');
+INSERT INTO StationHasRoute VALUES ('Avondale', 2, 'st_13');
+INSERT INTO StationHasRoute VALUES ('Kensington', 2, 'st_14');
+INSERT INTO StationHasRoute VALUES("Indian Creek", 2, "st_15");
+
+
+--  green west
+INSERT INTO StationHasRoute VALUES ('Candler Park', 3, 'st_1');
+INSERT INTO StationHasRoute VALUES ('Inman Park/Reynoldstown', 3, 'st_2');
+INSERT INTO StationHasRoute VALUES ('King Memorial', 3, 'st_3');
+INSERT INTO StationHasRoute VALUES ('Georgia State', 3, 'st_4');
+INSERT INTO StationHasRoute VALUES ('Five Points', 3, 'st_5');
+INSERT INTO StationHasRoute VALUES ('GWCC/Mercedes-Benz Stadium', 3, 'st_6');
+INSERT INTO StationHasRoute VALUES ('Vine City', 3, 'st_7');
+INSERT INTO StationHasRoute VALUES ('Ashby', 3, 'st_8');
+INSERT INTO StationHasRoute VALUES ('Bankhead', 3, 'st_9');
+
+-- green east
+INSERT INTO StationHasRoute VALUES ('Bankhead', 4, 'st_1');
+INSERT INTO StationHasRoute VALUES ('Ashby', 4, 'st_2');
+INSERT INTO StationHasRoute VALUES ('Vine City', 4, 'st_3');
+INSERT INTO StationHasRoute VALUES ('GWCC/Mercedes-Benz Stadium', 4, 'st_4');
+INSERT INTO StationHasRoute VALUES ('Five Points', 4, 'st_5');
+INSERT INTO StationHasRoute VALUES ('Georgia State', 4, 'st_6');
+INSERT INTO StationHasRoute VALUES ('King Memorial', 4, 'st_7');
+INSERT INTO StationHasRoute VALUES ('Inman Park/Reynoldstown', 4, 'st_8');
+INSERT INTO StationHasRoute VALUES ('Candler Park', 4, 'st_9');
+
+-- red south
+INSERT INTO StationHasRoute VALUES ('North Springs', 5, 'st_1');
+INSERT INTO StationHasRoute VALUES ('Sandy Springs', 5, 'st_2');
+INSERT INTO StationHasRoute VALUES ('Dunwoody', 5, 'st_3');
+INSERT INTO StationHasRoute VALUES ('Medical Center', 5, 'st_4');
+INSERT INTO StationHasRoute VALUES ('Buckhead', 5, 'st_5');
+INSERT INTO StationHasRoute VALUES ('Lindbergh Center', 5, 'st_6');
+INSERT INTO StationHasRoute VALUES ('Arts Center', 5, 'st_7');
+INSERT INTO StationHasRoute VALUES ('Midtown', 5, 'st_8');
+INSERT INTO StationHasRoute VALUES ('North Avenue', 5, 'st_9');
+INSERT INTO StationHasRoute VALUES ('Civic Center', 5, 'st_10');
+INSERT INTO StationHasRoute VALUES ('Peachtree Center', 5, 'st_11');
+INSERT INTO StationHasRoute VALUES ('Five Points', 5, 'st_12');
+INSERT INTO StationHasRoute VALUES ('Garnett', 5, 'st_13');
+INSERT INTO StationHasRoute VALUES ('West End', 5, 'st_14');
+INSERT INTO StationHasRoute VALUES ('Oakland City', 5, 'st_15');
+INSERT INTO StationHasRoute VALUES ('Lakewood', 5, 'st_16');
+INSERT INTO StationHasRoute VALUES ('East Point', 5, 'st_17');
+INSERT INTO StationHasRoute VALUES ('College Park', 5, 'st_18');
+INSERT INTO StationHasRoute VALUES ('Airport', 5, 'st_19');
+
+-- red north
+INSERT INTO StationHasRoute VALUES ('Airport', 6, 'st_1');
+INSERT INTO StationHasRoute VALUES ('College Park', 6, 'st_2');
+INSERT INTO StationHasRoute VALUES ('East Point', 6, 'st_3');
+INSERT INTO StationHasRoute VALUES ('Lakewood', 6, 'st_4');
+INSERT INTO StationHasRoute VALUES ('Oakland City', 6, 'st_5');
+INSERT INTO StationHasRoute VALUES ('West End', 6, 'st_6');
+INSERT INTO StationHasRoute VALUES ('Garnett', 6, 'st_7');
+INSERT INTO StationHasRoute VALUES ('Five Points', 6, 'st_8');
+INSERT INTO StationHasRoute VALUES ('Peachtree Center', 6, 'st_9');
+INSERT INTO StationHasRoute VALUES ('Civic Center', 6, 'st_10');
+INSERT INTO StationHasRoute VALUES ('North Avenue', 6, 'st_11');
+INSERT INTO StationHasRoute VALUES ('Midtown', 6, 'st_12');
+INSERT INTO StationHasRoute VALUES ('Arts Center', 6, 'st_13');
+INSERT INTO StationHasRoute VALUES ('Lindbergh Center', 6, 'st_14');
+INSERT INTO StationHasRoute VALUES ('Buckhead', 6, 'st_15');
+INSERT INTO StationHasRoute VALUES ('Medical Center', 6, 'st_16');
+INSERT INTO StationHasRoute VALUES ('Dunwoody', 6, 'st_17');
+INSERT INTO StationHasRoute VALUES ('Sandy Springs', 6, 'st_18');
+INSERT INTO StationHasRoute VALUES ('North Springs', 6, 'st_19');
+
+-- gold south
+INSERT INTO StationHasRoute VALUES ('Doraville', 7, 'st_1');
+INSERT INTO StationHasRoute VALUES ('Chamblee', 7, 'st_2');
+INSERT INTO StationHasRoute VALUES ('Brookhaven', 7, 'st_3');
+INSERT INTO StationHasRoute VALUES ('Lenox', 7, 'st_4');
+INSERT INTO StationHasRoute VALUES ('Lindbergh Center', 7, 'st_5');
+INSERT INTO StationHasRoute VALUES ('Arts Center', 7, 'st_6');
+INSERT INTO StationHasRoute VALUES ('Midtown', 7, 'st_7');
+INSERT INTO StationHasRoute VALUES ('North Avenue', 7, 'st_8');
+INSERT INTO StationHasRoute VALUES ('Civic Center', 7, 'st_9');
+INSERT INTO StationHasRoute VALUES ('Peachtree Center', 7, 'st_10');
+INSERT INTO StationHasRoute VALUES ('Five Points', 7, 'st_11');
+INSERT INTO StationHasRoute VALUES ('Garnett', 7, 'st_12');
+INSERT INTO StationHasRoute VALUES ('West End', 7, 'st_13');
+INSERT INTO StationHasRoute VALUES ('Oakland City', 7, 'st_14');
+INSERT INTO StationHasRoute VALUES ('Lakewood', 7, 'st_15');
+INSERT INTO StationHasRoute VALUES ('East Point', 7, 'st_16');
+INSERT INTO StationHasRoute VALUES ('College Park', 7, 'st_17');
+INSERT INTO StationHasRoute VALUES ('Airport', 7, 'st_18');
+
+-- gold north
+INSERT INTO StationHasRoute VALUES ('Airport', 8, 'st_1');
+INSERT INTO StationHasRoute VALUES ('College Park', 8, 'st_2');
+INSERT INTO StationHasRoute VALUES ('East Point', 8, 'st_3');
+INSERT INTO StationHasRoute VALUES ('Lakewood', 8, 'st_4');
+INSERT INTO StationHasRoute VALUES ('Oakland City', 8, 'st_5');
+INSERT INTO StationHasRoute VALUES ('West End', 8, 'st_6');
+INSERT INTO StationHasRoute VALUES ('Garnett', 8, 'st_7');
+INSERT INTO StationHasRoute VALUES ('Five Points', 8, 'st_8');
+INSERT INTO StationHasRoute VALUES ('Peachtree Center', 8, 'st_9');
+INSERT INTO StationHasRoute VALUES ('Civic Center', 8, 'st_10');
+INSERT INTO StationHasRoute VALUES ('North Avenue', 8, 'st_11');
+INSERT INTO StationHasRoute VALUES ('Midtown', 8, 'st_12');
+INSERT INTO StationHasRoute VALUES ('Arts Center', 8, 'st_13');
+INSERT INTO StationHasRoute VALUES ('Lindbergh Center', 8, 'st_14');
+INSERT INTO StationHasRoute VALUES ('Lenox', 8, 'st_15');
+INSERT INTO StationHasRoute VALUES ('Brookhaven', 8, 'st_16');
+INSERT INTO StationHasRoute VALUES ('Chamblee', 8, 'st_17');
+INSERT INTO StationHasRoute VALUES ('Doraville', 8, 'st_18');
